a20832b04f727e2af6f5a85aacb7e004ee23683c
Chika0IQ <mohdkhairin62@gmail.com>
2020-12-10 13:38:14 +0800

    Changes(Khai)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody playerRb;
+    float speed 4.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayerMove()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+}
