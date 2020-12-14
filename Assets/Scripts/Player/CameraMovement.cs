15de31dbb21d0050a3ac7942566c36141e7a8247
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2020-12-14 11:55:31 +0800

    Ben's lvl3

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -6,7 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
 
     public GameObject player;
-    Vector3 offset = new Vector3(0.0f, 2.33f, -3.32f);
+    Vector3 offset = new Vector3(0.0f, 3f, -4f);
     // Start is called before the first frame update
     void Start()
     {
