e24a6063c3b79af734b382f39cc9901a84c110bf
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2020-12-15 19:05:38 +0800

    FasterSpeed

--- Assets/Scripts/Level3/CylinderRotater.cs
+++ Assets/Scripts/Level3/CylinderRotater.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CylinderRotater : MonoBehaviour
 {
-    float Rotatespeed = 50.0f;
+    float Rotatespeed = 100.0f;
     // Start is called before the first frame update
     void Start()
     {
