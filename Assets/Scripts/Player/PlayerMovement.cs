33f6d3172f9f7b9ab38735ebf2656ee78ca7fd66
Chika0IQ <mohdkhairin62@gmail.com>
2020-12-17 11:35:27 +0800

    Moving LosingCondition Into Fucntion and Commeting more on PlayerMovementScritp - (Khai)
    Moving LosingCondition Into Fucntion better performan and Commeting more on PlayerMovementScritpt - (Khai)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,93 +5,69 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //  VARIABLES  //
+    public Rigidbody playerRb;       // Set rigidbody of player
+    public Animator animator;        // Set Animator for player
+    public Transform highSlope;      // Set to rotate the Slope on Level 4
+                                     
+    private float speed = 5.0f;      // Speed for player 
+    private float jump = 7.5f;       // jump force of player
+    private float gravity = 850f;    // Set gravity for player to land with force
 
-    public Rigidbody playerRb;
-    public Animator animator;
-    public Transform highSlope;
+    private int spacePressed = 0;    // Count amount of SpaceBar pressed
+    private int touchSwitch = 1;     // Count if player touching on CubeSwitch on Level4
 
-    private float speed = 5.0f;
-    private float jump = 7.5f;
-    private float gravity = 850f;
-
-    private int spacePressed = 0;
-    private int touchSwitch = 1;
-
-    private bool switchBool = false;
+    private bool switchBool = false; // Boolean to enable or disable the CubeSwitch
 
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        //Physics.gravity *= gravityModifier;
-        playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();  // Get Animator commponent 
+        playerRb = GetComponent<Rigidbody>(); // Get Rigidbody commponent 
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerMove();
+        PlayerMove();    // Call PlayerMove Function
+        LoseCondition(); // Call LoseCondition Function When met
 
-        //Losing Condition
-        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "FirstLevel")//Restart First Level
-        {
-            print("You Lose!");
-            SceneManager.LoadScene("LoseScene");
-        }
-
-        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "SecondLevel")//Restart Second Level
-        {
-            print("You Lose!");
-            SceneManager.LoadScene("LoseScene2");
-        }
-
-        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "ThirdLevel")//Restart Third Level
-        {
-            print("You Lose!");
-            SceneManager.LoadScene("LoseScene3");
-        }
-
-        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "FourthLevel")//Restart Fourth Level
-        {
-            print("You Lose!");
-            SceneManager.LoadScene("LoseScene4");
-        }
-
-        if (touchSwitch == 0 && switchBool == true)//Enable the Slope to rotate
+        if (touchSwitch == 0 && switchBool == true) // Enable the Slope to rotate when bool is true
         {
             highSlope.GetComponent<Transform>().Rotate(-20f, 0, 0f);
             touchSwitch += 1;
         }
     }
 
-    private void PlayerMove()
+    private void PlayerMove() // Player Movement Function
     {
 
-        playerRb.AddForce(Vector3.down * Time.deltaTime * gravity);// Fixed gravity
+        playerRb.AddForce(Vector3.down * Time.deltaTime * gravity); // Fixed gravity
 
-        if (Input.GetKey(KeyCode.W))//Forward
+        if (Input.GetKey(KeyCode.W)) // Player moving Forward and Animation
         {
             PlayerAnimAndForward();
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-        if (Input.GetKey(KeyCode.A))//Left
+        if (Input.GetKey(KeyCode.A)) // Player moving Left and Animation
         {
             PlayerAnimAndForward();
             transform.rotation = Quaternion.Euler(0, 270, 0);
         }
-        if (Input.GetKey(KeyCode.S))//Back
+        if (Input.GetKey(KeyCode.S)) // Player moving Backwards and Animation
         {
             PlayerAnimAndForward();
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-        if (Input.GetKey(KeyCode.D))//Right
+        if (Input.GetKey(KeyCode.D)) // Player moving Right and Animation
         {
             PlayerAnimAndForward();
             transform.rotation = Quaternion.Euler(0, 90, 0);
         }
 
-        else if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))//idle anim
+        // Idle animation when key Released
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) 
         {
             animator.SetBool("isRun", false);
         }
@@ -103,7 +79,7 @@ public class PlayerMovement : MonoBehaviour
             spacePressed += 1;
         }
 
-        //Sprint
+        // LeftShift Sprint and speed of sprint
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
             speed = 8.0f;
@@ -114,37 +90,72 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void PlayerAnimAndForward()//Run anim
+    private void PlayerAnimAndForward()// Run animation and player run
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         animator.SetBool("isRun", true);
     }
 
+    private void LoseCondition() // Losing Condition for Each Level
+    {
+        // Check if player is on Level1 and Restart First Level
+        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "FirstLevel")
+        {
+            print("You Lose!");
+            SceneManager.LoadScene("LoseScene");
+        }
+
+        // Check if player is on Level2 and Restart Second Level
+        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "SecondLevel")
+        {
+            print("You Lose!");
+            SceneManager.LoadScene("LoseScene2");
+        }
+
+        // Check if player is on Level3 and Restart Third Level
+        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "ThirdLevel")
+        {
+            print("You Lose!");
+            SceneManager.LoadScene("LoseScene3");
+        }
+
+        // Check if player is on Level4 and Restart Fourth Level
+        if (transform.position.y < -5 && SceneManager.GetActiveScene().name == "FourthLevel")
+        {
+            print("You Lose!");
+            SceneManager.LoadScene("LoseScene4");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("FinishLineLvl4"))//Transition to Win Scene
-        {
-            SceneManager.LoadScene("WinScene");
-        }
-
-        if (other.gameObject.CompareTag("FinishLineLvl2"))//Transition to 3rd Level
-        {
-            SceneManager.LoadScene("ThirdLevel");
-        }
-
+        // Transition to 1st Level
         if (other.gameObject.CompareTag("FinishLineLvl1"))
         {
             SceneManager.LoadScene("SecondLevel");
         }
 
-        if (other.gameObject.CompareTag("SlopeSwitch"))//Toggle Switch
+        // Transition to 3rd Level
+        if (other.gameObject.CompareTag("FinishLineLvl2"))
+        {
+            SceneManager.LoadScene("ThirdLevel");
+        }
+
+        // Transition to Win Scene
+        if (other.gameObject.CompareTag("FinishLineLvl4"))
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+
+        // Toggle Switch for CubeSwitch to ON or OFF
+        if (other.gameObject.CompareTag("SlopeSwitch"))// Toggle Switch for CubeSwitch to ON or OFF
         {
             touchSwitch -= 1;
             switchBool = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)//Enable the switch to be toggled only once
+    private void OnTriggerExit(Collider other)// Enable the switch to be toggled only once
     {
         if (other.gameObject.CompareTag("SlopeSwitch"))
         {
@@ -153,7 +164,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)//Check Collision Condition for GamePlatforms
+    private void OnCollisionEnter(Collision collision)// Check Collision Condition for GamePlatforms
     {
         if(collision.gameObject.CompareTag("GamePlatforms"))
         {
@@ -161,7 +172,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay(Collision collision)//Check Collision Condition for DropPlatforms
+    private void OnCollisionStay(Collision collision)// Check Collision Condition for DropPlatforms
     {
         if (collision.gameObject.CompareTag("DropPlatform"))
         {
